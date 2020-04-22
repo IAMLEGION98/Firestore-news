6b9ce450ee08cb59eed00e1ba0889813b3f42005
IAMLEGION98 <anupamrajanish@gmail.com>
2020-04-22 20:54:42 +0530

    Layout view added

--- app/src/main/java/com/example/visionnewsb1/Adapter/NewsAdapter.java
+++ app/src/main/java/com/example/visionnewsb1/Adapter/NewsAdapter.java
@@ -1,4 +1,19 @@
 package com.example.visionnewsb1.Adapter;
 
-public class NewsAdapter {
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class NewsAdapter extends PagerAdapter {
+    @Override
+
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return false;
+    }
 }
