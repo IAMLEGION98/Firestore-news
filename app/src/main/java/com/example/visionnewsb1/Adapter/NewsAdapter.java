fda9d7f6ba3eaa937747828f250f00a683b693a6
IAMLEGION98 <anupamrajanish@gmail.com>
2020-04-22 23:04:15 +0530

    Working First Build Ready

--- app/src/main/java/com/example/visionnewsb1/Adapter/NewsAdapter.java
+++ app/src/main/java/com/example/visionnewsb1/Adapter/NewsAdapter.java
@@ -1,19 +1,76 @@
 package com.example.visionnewsb1.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.visionnewsb1.Modal.News;
+import com.example.visionnewsb1.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 public class NewsAdapter extends PagerAdapter {
-    @Override
 
+    Context context;
+    List<News> newsList;
+    LayoutInflater inflater;
+
+    public NewsAdapter(Context context, List<News> newsList) {
+        this.context = context;
+        this.newsList = newsList;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
     public int getCount() {
-        return 0;
+        return newsList.size();
     }
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return false;
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        ((ViewPager)container).removeView((View)object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = inflater.inflate(R.layout.view_pager_item,container,false);
+        ImageView news_image = (ImageView)view.findViewById(R.id.news_image);
+        TextView news_heading = (TextView)view.findViewById(R.id.news_heading);
+        TextView news_description = (TextView)view.findViewById(R.id.news_description);
+        FloatingActionButton btn_fav = (FloatingActionButton)view.findViewById(R.id.btn_fav);
+        btn_fav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,"Liked",Toast.LENGTH_SHORT).show();
+            }
+        });
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,"Clicked NEws",Toast.LENGTH_SHORT).show();
+            }
+        });
+        Picasso.get().load(newsList.get(position).getImage()).into(news_image);
+        news_heading.setText(newsList.get(position).getHeading());
+        news_description.setText(newsList.get(position).getDescription());
+
+        container.addView(view);
+        return view;
     }
 }
--- app/src/main/java/com/example/visionnewsb1/MainActivity.java
+++ app/src/main/java/com/example/visionnewsb1/MainActivity.java
@@ -1,14 +1,88 @@
 package com.example.visionnewsb1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.visionnewsb1.Adapter.NewsAdapter;
+import com.example.visionnewsb1.Interface.IFireStoreLoadDone;
+import com.example.visionnewsb1.Modal.News;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
+
+public class MainActivity extends AppCompatActivity implements IFireStoreLoadDone {
+
+    ViewPager viewPager;
+    NewsAdapter newsAdapter;
+    IFireStoreLoadDone iFireStoreLoadDone;
+
+    CollectionReference news;
+    AlertDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iFireStoreLoadDone = this;
+        news = FirebaseFirestore.getInstance().collection("news");
+
+        dialog = new SpotsDialog.Builder().setContext(this).build();
+        viewPager = (ViewPager)findViewById(R.id.view_pager);
+        getData();
+    }
+
+    private void getData() {
+         dialog.show();
+        news.get().addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                iFireStoreLoadDone.onFireSoreLoadFailed(e.getMessage());
+            }
+        })
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful())
+                        {
+                            List<News> newslist = new ArrayList<>();
+                            for(QueryDocumentSnapshot newssnapshot : task.getResult())
+                            {
+                                News news = newssnapshot.toObject(News.class);
+                                newslist.add(news);
+
+                            }
+                            iFireStoreLoadDone.onFireStoreLoadSuccess(newslist);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onFireStoreLoadSuccess(List<News> news) {
+       newsAdapter = new NewsAdapter(this,news);
+       viewPager.setAdapter(newsAdapter);
+       dialog.dismiss();
+    }
+
+    @Override
+    public void onFireSoreLoadFailed(String message) {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+        dialog.dismiss();
+
     }
 }
